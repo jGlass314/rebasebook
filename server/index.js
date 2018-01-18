91afce175755b9d62bb0a63f62766a1a80317132
acenine <acenine@gmx.com>
2018-01-18 15:34:03 -0800

    Add passport to project

--- server/index.js
+++ server/index.js
@@ -10,6 +10,30 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const passport = require('passport') 
+const LocalStrategy = require('passport-local').Strategy;
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+
+  // app.use(express.cookieParser());
+  // app.use(express.session({ secret: 'keyboard cat' }));
+
 //serve static pages
 app.use(express.static(__dirname + '/../client/dist'));
 app.use('/:username', express.static(__dirname + '/../client/dist'));
@@ -17,6 +41,13 @@ app.use('/:username', express.static(__dirname + '/../client/dist'));
 //handle /api endpoints
 app.use('/api', api);
 
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/api/' + req.user.username);
+});
 
 let port = process.env.PORT || 3000;
 
